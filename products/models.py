09497df81fa0e85c786ed46bf1f502360ae99c37
smenendez <smenendez@localhost.localdomain>
2026-07-03 14:51:02 +0200

    class.v20_01

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.TextField(max_length=200, verbose_name="nombre")
+    description = models.TextField(max_length=300, verbose_name="descripción")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="precio")
+    available = models.BooleanField(default=True, verbose_name="disponible")
+    photo = models.ImageField(upload_to="logos", blank=True, null=True, verbose_name="foto")
+
+    def __str__(self):
+        return self.name
