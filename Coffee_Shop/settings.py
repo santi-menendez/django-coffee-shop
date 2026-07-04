abf625a6aa7fb8e5ece3d449195e2d77dcb7bf63
santi-menendez <santi.menendez@upc.edu>
2026-07-04 22:11:07 +0200

    class32_v01

--- Coffee_Shop/settings.py
+++ Coffee_Shop/settings.py
@@ -13,9 +13,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
